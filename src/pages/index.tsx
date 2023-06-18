2dd5c812358b0d9a9882f207ee70fb886073ea5b
kervint1 <s1f102302124@iniad.org>
2023-06-19 02:18:41 +0900

    kuhakurensa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,151 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  const [userInputs, setUserInputs] = useState<(0 | 1 | 2 | 3)[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const bombcount = 10;
+
+  const [bombMap, setBombMap] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  function getrandom(mn: number, mx: number) {
+    const random = Math.floor(Math.random() * (mx + 1 - mn) + mn);
+    return random;
+  }
+  function count_userinputs(a: number) {
+    let num = 0;
+    for (let y = 0; y <= 8; y++) {
+      for (let x = 0; x <= 8; x++) {
+        if (userInputs[y][x] === a) {
+          num += 1;
+        }
+      }
+    }
+    return num;
+  }
+  function count_bombmap(a: number) {
+    let num = 0;
+    for (let y = 0; y <= 8; y++) {
+      for (let x = 0; x <= 8; x++) {
+        if (bombMap[y][x] === a) {
+          num++;
+        }
+      }
+    }
+    return num;
+  }
+  const board: number[][] = [];
+  for (let a = 1; a <= 9; a++) {
+    board.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  }
+
+  while (Number(count_userinputs(1)) === 1 && Number(count_bombmap(1)) < 10) {
+    let decidey = getrandom(0, 9);
+    let decidex = getrandom(0, 9);
+    if (userInputs[decidey][decidex] !== 1) {
+      bombMap[decidey][decidex] = 1;
+    }
+    setBombMap(bombMap);
+  }
+  const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
+  // const isFailure = userInputs.some((row, y) =>
+  //   row.some((input, x) => input === 1 && bombMap[y][x] === 1)
+  // );
+
+  const clickcell = (x: number, y: number) => {
+    console.log(x, y);
+    const newBoard: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
+    newBoard[y][x] = 1;
+    setUserInputs(newBoard);
+  };
+  // let zeroList: { x: number; y: number }[] = [];
+  // for () {
+  //   zeroList = // board + directions + userInputs + bombMap
+  // }
+  for (let a = 1; a <= 10; a += 1) {
+    for (let y = 0; y <= 8; y++) {
+      for (let x = 0; x <= 8; x++) {
+        if (board[y][x] === 9) {
+          for (let movey = -1; movey <= 1; movey++) {
+            for (let movex = -1; movex <= 1; movex++) {
+              if (x === 0 && y === 0) {
+              } else if (
+                userInputs[y + movey] !== undefined &&
+                userInputs[x + movex] !== undefined
+              ) {
+                userInputs[y + movey][x + movex] = 1;
+              }
+            }
+          }
+        }
+      }
+    }
+    for (let y = 0; y <= 8; y++) {
+      for (let x = 0; x <= 8; x++) {
+        if (userInputs[y][x] === 1 && bombMap[y][x] !== 1) {
+          let numb = 0;
+          for (let movey = -1; movey <= 1; movey++) {
+            for (let movex = -1; movex <= 1; movex++) {
+              if (x === 0 && y === 0) {
+              } else if (
+                bombMap[y + movey] !== undefined &&
+                bombMap[x + movex] !== undefined &&
+                bombMap[y + movey][x + movex] === 1
+              ) {
+                numb++;
+              }
+            }
+          }
+          if (numb === 0) {
+            numb = 9;
+          }
+          board[y][x] = numb;
+        } else if (userInputs[y][x] === 1 && bombMap[y][x] === 1) {
+          board[y][x] = 10;
+        }
+      }
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <div className={styles.hoge} />
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickcell(x, y)}>
+              {color === 0 && <div className={styles.nopush}></div>}
+              {color === 1 && <div className={styles.one}></div>}
+              {color === 2 && <div className={styles.two}></div>}
+              {color === 3 && <div className={styles.three}></div>}
+              {color === 4 && <div className={styles.four}></div>}
+              {color === 5 && <div className={styles.five}></div>}
+              {color === 6 && <div className={styles.six}></div>}
+              {color === 7 && <div className={styles.seven}></div>}
+              {color === 8 && <div className={styles.eight}></div>}
+              {color === 9 && <div className={styles.nothing}></div>}
+              {color === 10 && <div className={styles.bomb}></div>}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
