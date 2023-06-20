e41f304728e4b65445cbb8b9e584e9dcebe193a7
kervint1 <s1f102302124@iniad.org>
2023-06-20 20:33:53 +0900

    数字押し機能

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,13 +26,14 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-
-  const resset: number[][] = [];
-  resset.push([0]);
+  //リセットセル
+  const reset: number[][] = [];
+  reset.push([0]);
   function getrandom(mn: number, mx: number) {
     const random = Math.floor(Math.random() * (mx + 1 - mn) + mn);
     return random;
   }
+  //userinputsのある数aを数える
   function count_userinputs(a: number) {
     let num = 0;
     for (let y = 0; y <= 8; y++) {
@@ -44,6 +45,7 @@ const Home = () => {
     }
     return num;
   }
+  //bombmapのある数aを数える
   function count_bombmap(a: number) {
     let num = 0;
     for (let y = 0; y <= 8; y++) {
@@ -55,23 +57,23 @@ const Home = () => {
     }
     return num;
   }
-
+  //クリックしてリセット
   const resetclickcell = (x: number, y: number) => {
     console.log(x, y);
     const inputsBoard: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
     const bombBoard: number[][] = JSON.parse(JSON.stringify(board));
-    if (isFailure === true || resset[0][0] === 2) {
-      for (let y = 0; y <= 8; y++) {
-        for (let x = 0; x <= 8; x++) {
-          inputsBoard[y][x] = 0;
-          bombBoard[y][x] = 0;
-        }
+    for (let y = 0; y <= 8; y++) {
+      for (let x = 0; x <= 8; x++) {
+        inputsBoard[y][x] = 0;
+        bombBoard[y][x] = 0;
       }
-      setUserInputs(inputsBoard);
-      setBombMap(bombBoard);
     }
+    setUserInputs(inputsBoard);
+    setBombMap(bombBoard);
   };
+  //右クリック
   const onclickcell = (x: number, y: number) => {
+    document.getElementsByTagName('html')[0].oncontextmenu = () => false;
     console.log(x, y);
     const newBoard: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
     if (userInputs[y][x] === 0) {
@@ -83,12 +85,13 @@ const Home = () => {
     }
     setUserInputs(newBoard);
   };
-
+  //boardを0リストにする
   const board: number[][] = [];
   for (let a = 1; a <= 9; a++) {
     board.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
   }
 
+  //地雷を生成
   while (Number(count_userinputs(1)) === 1 && Number(count_bombmap(1)) < 10) {
     const decidey = getrandom(0, 8);
     const decidex = getrandom(0, 8);
@@ -97,37 +100,7 @@ const Home = () => {
     }
     setBombMap(bombMap);
   }
-  // const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
-  const isFailure = userInputs.some((row, y) =>
-    row.some((input, x) => input === 1 && bombMap[y][x] === 1)
-  );
-
-  const clickcell = (x: number, y: number) => {
-    console.log(x, y);
-    const newBoard: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
-    if (userInputs[y][x] === 0 && isFailure !== true) {
-      newBoard[y][x] = 1;
-    }
-    setUserInputs(newBoard);
-  };
-  // let zeroList: { x: number; y: number }[] = [];
-  // for () {
-  //   zeroList = // board + directions + userInputs + bombMap
-  // }
-  if (isFailure === true) {
-    resset[0][0] = 1;
-  } else if (count_userinputs(0) + count_userinputs(2) + count_userinputs(3) === bombcount) {
-    resset[0][0] = 2;
-  }
-  for (let y = 0; y <= 8; y++) {
-    for (let x = 0; x <= 8; x++) {
-      if (userInputs[y][x] === 2) {
-        board[y][x] = 11;
-      } else if (userInputs[y][x] === 3) {
-        board[y][x] = 12;
-      }
-    }
-  }
+  //boardを設定
   if (count_bombmap(1) >= 10) {
     for (let a = 1; a <= 10; a += 1) {
       for (let y = 0; y <= 8; y++) {
@@ -176,12 +149,68 @@ const Home = () => {
       }
     }
   }
+  // const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
+  const isFailure = userInputs.some((row, y) =>
+    row.some((input, x) => input === 1 && bombMap[y][x] === 1)
+  );
+  //クリック時
+  let numclear = 0;
+  const clickcell = (x: number, y: number) => {
+    console.log(x, y);
+    const newUserInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
+    //箱開く
+
+    if (userInputs[y][x] === 0 && !isFailure) {
+      newUserInputs[y][x] = 1;
+    }
+    //数字クリック時周り開く
+    else if (
+      userInputs[y][x] === 1 &&
+      userInputs
+        .map((row) => row.slice(Math.max(0, x - 1), Math.min(8, x + 1) + 1))
+        .slice(Math.max(0, y - 1), Math.min(8, y + 1) + 1)
+        .flat()
+        .filter((n) => n === 2).length === board[y][x] &&
+      !isFailure
+    ) {
+      for (let movey = -1; movey <= 1; movey++) {
+        for (let movex = -1; movex <= 1; movex++) {
+          if (
+            userInputs[y + movey] !== undefined &&
+            userInputs[y + movey][x + movex] !== undefined &&
+            userInputs[y + movey][x + movex] === 0
+          ) {
+            newUserInputs[y + movey][x + movex] = 1;
+          }
+        }
+      }
+    }
+    setUserInputs(newUserInputs);
+  };
+  //地雷踏んだ
+  if (isFailure === true) {
+    reset[0][0] = 1;
+  }
+  //サングラスフェイス
+  else if (count_userinputs(0) + count_userinputs(2) + count_userinputs(3) === bombcount) {
+    reset[0][0] = 2;
+  }
+  //flag=11,?=12
+  for (let y = 0; y <= 8; y++) {
+    for (let x = 0; x <= 8; x++) {
+      if (userInputs[y][x] === 2) {
+        board[y][x] = 11;
+      } else if (userInputs[y][x] === 3) {
+        board[y][x] = 12;
+      }
+    }
+  }
 
   return (
     <div className={styles.container}>
       <div className={styles.plate}>
         <div className={styles.reset}>
-          {resset.map((row, y) =>
+          {reset.map((row, y) =>
             row.map((color, x) => (
               <div className={styles.reset} key={`${x}-${y}`} onClick={() => resetclickcell(x, y)}>
                 {color === 0 && <div className={styles.smile} />}
