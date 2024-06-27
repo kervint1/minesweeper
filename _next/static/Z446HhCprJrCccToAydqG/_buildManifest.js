252e9f1cdc2164554933c6cb43548031d957cc70
kervint1 <kervint1@users.noreply.github.com>
2024-06-27 10:58:19 +0000

    deploy: a0126ae44b8e478594c3efea3718cc41f0bbe7a6

---
+++ _next/static/Z446HhCprJrCccToAydqG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ee6736b8a50aa46c.css","static/chunks/pages/index-1f6c7943f9cc82ed.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
